f6504a60c875ea9c42fc0a207cb3619e0661248a
helenou <ln.lejeune@gmail.com>
2018-05-19 08:41:24 +0200

    mise en place d'un factory ManagerFactory

--- ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResource.java
+++ ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResource.java
@@ -1,12 +1,14 @@
 package org.example.demo.ticket.webapp.rest.resource.projet;
 
 import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.example.demo.ticket.business.ManagerFactory;
 import org.example.demo.ticket.business.manager.ProjetManager;
 import org.example.demo.ticket.model.bean.projet.Projet;
 import org.example.demo.ticket.model.exception.NotFoundException;
@@ -21,6 +23,7 @@ import org.example.demo.ticket.model.exception.NotFoundException;
 @Produces(MediaType.APPLICATION_JSON)
 public class ProjetResource {
 
+	private ManagerFactory managerFactory;
     /**
      * Renvoie le {@link Projet} d'identifiant {@code pId}
      *
@@ -32,7 +35,7 @@ public class ProjetResource {
     @Path("{id}")
     public Projet get(@PathParam("id") Integer pId) throws NotFoundException {
         ProjetManager vProjetManager = new ProjetManager();
-        Projet vProjet = vProjetManager.getProjet(pId);
+        Projet vProjet = managerFactory.getProjetManager().getProjet(pId);
         return vProjet;
     }
 
